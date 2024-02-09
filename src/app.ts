6dad251317ccf126b086182063eee744f4fbac3f
Danilo Raposo <daniloraposocaldas@hotmail.com>
2024-02-09 15:58:16 -0300

    app (feat): add middleware error handler

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import type {Express} from "express"
+import type {Express, Request, Response, NextFunction} from "express"
+import type { TError } from "./utils/error";
 
 import authRouter from "./routes/auth";
 
@@ -23,6 +24,11 @@ app.use((req, res, next) => {
 });
 
 app.use("/auth", authRouter);
+
+// middleware error handler
+app.use((error: TError, req: Request, res: Response, next: NextFunction) => {
+  res.status(error.statusCode ?? 500).json({message: error.message, data: error?.data});
+})
 
 mongoose.connect(process.env.MONGO_URI as string).then(() => {
   app.listen(process.env.PORT);
