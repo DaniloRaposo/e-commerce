e13b1924e112861197052ad93d971bd33b880b61
Danilo Raposo <daniloraposocaldas@hotmail.com>
2024-03-22 09:45:26 -0300

    product (feat): add time stamp into product model

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,35 +1,38 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 
 interface TProduct {
-  name: string,
-  price: number,
-  description: string,
-  imageUrl: string,
-  userId: mongoose.Types.ObjectId,
+  name: string;
+  price: number;
+  description: string;
+  imageUrl: string;
+  userId: mongoose.Types.ObjectId;
 }
 
-const productSchema = new mongoose.Schema<TProduct>({
-  name: {
-    type: String,
-    required: true,
+const productSchema = new mongoose.Schema<TProduct>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
-  price: {
-    type: Number,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  imageUrl: {
-    type: String,
-    required: true,
-  },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  }
-})
+  { timestamps: true }
+);
 
 export default mongoose.model("Product", productSchema);
